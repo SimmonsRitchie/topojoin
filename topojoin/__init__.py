493acfbe9fc10bfe6c59b80583b419194168a7d7
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-08-02 12:35:15 -0400

    tweaks

--- topojoin/__init__.py
+++ topojoin/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for TopoJoin."""
 
-__author__ = """DSR"""
-__email__ = 'DSR'
-__version__ = '0.1.0'
+__author__ = "DSR"
+__email__ = "info@simmonsritchie.com"
+__version__ = "0.1.0"
