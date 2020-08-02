b84205789c8192e6277f889401c50ab9d547d4d6
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-08-02 10:58:42 -0400

    New tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,22 @@ root = Path(__file__).parent.resolve()
 
 
 @pytest.fixture(scope="session")
-def csv_path():
+def csv_path() -> str:
+    """
+    Returns a path to a CSV file. The file has a column called 'fips' representing unique county IDs. It joins
+    with a column called 'GEOID' in the topojson file at topo_path.
+    """
     return str(root / "fixtures/pa-county-pop.csv")
+
+
+@pytest.fixture(scope="session")
+def csv_path_non_matching():
+    """
+    Returns a path to a CSV file. The file has a column called 'fips' representing unique county IDs. It joins
+    with a column called 'GEOID' in the topojson file at topo_path. However, this file is missing rows so it will not
+    match cleanly with the topojson.
+    """
+    return str(root / "fixtures/pa-county-pop__non-matching-rows.csv")
 
 
 @pytest.fixture(scope="session")
